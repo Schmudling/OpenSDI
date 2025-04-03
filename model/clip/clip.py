dc4c9c96bffc1f49711c47eb007364d9dcbaaae2
Schmudling <34129880+Schmudling@users.noreply.github.com>
2025-04-03 21:58:01 +0200

    Update clip.py

--- model/clip/clip.py
+++ model/clip/clip.py
@@ -37,7 +37,7 @@ _MODELS = {
 }
 
 
-def _download(url: str, root: str = os.path.expanduser("~/.cache/clip")):
+def _download(url: str, root: str = os.path.expanduser("/content/weights")):
     os.makedirs(root, exist_ok=True)
     filename = os.path.basename(url)
 
